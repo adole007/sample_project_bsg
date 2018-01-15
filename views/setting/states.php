dec26226ce6f58121f6529c302bcaff17ae1fb39
Stephen Igwue <olibenu@yahoo.com>
2018-01-15 16:09:24 +0100

    added delete confirmation for states

--- views/setting/states.php
+++ views/setting/states.php
@@ -1,5 +1,5 @@
 <?php
-//require_once(__DIR__ . '/../../models/autoload.php');
+
 State::$context = $context;
 State::$params = $param1; //or an array
 State::pre();
@@ -58,20 +58,17 @@ State::pre();
     <hr />
     <form method="POST" action="<?= CONTROLLER ?>state/delete<?= '.php' ?>">
         <div class="row">
-            <div class="col-lg-3">
-               <b>ID</b>
-               <input name="id" type="text" class="form-control" placeholder="ID" />
-            </div>
-            <div class="col-lg-9">
-               <b>State</b>
-               <input name="state" type="text" class="form-control" placeholder="State" />
+            <div class="col-lg-6">
+               <b>Confirmation</b>
+               <p>Are you sure you want to delete?</p>
             </div>
         </div>
         <br />
 
         <!-- /button function for this form -->
         <div class="form-group">
-          <button type="submit" class="btn btn-danger">Delete</button>
+          <button type="submit" class="btn btn-danger">Yes</button>
+          <a href="#" class="btn btn-default">No</a>
         </div>
         <br />
     </form>
@@ -98,7 +95,7 @@ State::pre();
                     <td><?= $details['state'] ?></td>
                     <td>
                         <a href="<?= $_SERVER['REQUEST_URI'] ?>/edit/<?= $details['id'] ?>" class="btn btn-default">Edit</a>
-                        <a class="btn btn-danger">Delete</a>
+                        <a href="<?= $_SERVER['REQUEST_URI'] ?>/delete/<?= $details['id'] ?>" class="btn btn-danger">Delete</a>
                     </td>
                 </tr>
             <?php ++$index; endforeach ?>
